c1efa2c63ed0237cf7293b360a40fe5772cdae8f
Ritik Sharma <ritiksharma@BLR.ALLEGISINDIA.COM>
2022-06-10 16:16:18 +0530

    First commit

---
+++ ICIN Bank/UserFront/src/test/java/com/userfrony/UserFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userfrony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
